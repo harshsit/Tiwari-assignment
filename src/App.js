e8992337c661314a0c587121b6c3fff57b306c66
harshsit <harsh.raj@sitpune.edu.in>
2021-02-24 16:07:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+        name:'',
+
+  }
+  var str = document.getElementById("id1"); 
+  if (str.value ==null) { 
+      str.focus(); 
+      return; 
+  } 
+  var a = "ASCII Code is == >  "; 
+  document.getElementById("demo").innerHTML 
+      = a + str.value.charCodeAt(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> 
+        Enter any character: 
+        <input type="text" id="id1" 
+            name="text1" maxLength="1"/> 
+    </p> 
+  
+    <button onclick="myFunction()"> 
+        Get ASCII code 
+    </button> 
+  
+    <p id="demo" style="color:red;"> </p>
     </div>
   );
+}
 }
 
 export default App;
